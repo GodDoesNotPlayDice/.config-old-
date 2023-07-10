5f7c1474aacc8b26d00c168ef1afd7e0b53217ab
Vicente Andres Vasquez Gonzalez <104604407+GodDoesNotPlayDice@users.noreply.github.com>
2023-07-10 12:57:16 -0400

    init dots
    init dots

---
+++ fish/conf.d/mocha.fish
@@ -0,0 +1,38 @@
+# Dracula colors
+set -U fish_color_normal normal
+set -U fish_color_command f8f8f2
+set -U fish_color_quote f1fa8c
+set -U fish_color_redirection 8be9fd
+set -U fish_color_end 50fa7b
+set -U fish_color_error ffb86c
+set -U fish_color_param ff79c6
+set -U fish_color_comment 6272a4
+set -U fish_color_match --background=brblack
+set -U fish_color_selection white --bold --background=brblue
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion bd93f9
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description b3a06d
+set -U fish_pager_color_selected_background --background=brblack
+set -U fish_pager_color_secondary_completion 
+set -U fish_pager_color_secondary_background 
+set -U fish_color_option 
+set -U fish_color_host_remote 
+set -U fish_pager_color_background 
+set -U fish_pager_color_secondary_description 
+set -U fish_color_keyword 
+set -U fish_pager_color_secondary_prefix 
+set -U fish_pager_color_selected_description 
+set -U fish_pager_color_selected_prefix 
+set -U fish_pager_color_selected_completion 
